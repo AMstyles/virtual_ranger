78d56f8f7229ce84f12601fc994fa9952e669a13
Arinaho Muleba <arinaomuleluz@gmail.com>
2022-08-06 01:27:49 +0200

    second commit

---
+++ lib/models/preset_styles.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const drawerTextStyle =
+    TextStyle(color: Colors.white, fontSize: 26, fontWeight: FontWeight.w200);
---
+++ lib/pages/news_and_deals_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewsAndDealsPage extends StatefulWidget {
+  NewsAndDealsPage({Key? key}) : super(key: key);
+
+  @override
+  State<NewsAndDealsPage> createState() => _NewsAndDealsPageState();
+}
+
+class _NewsAndDealsPageState extends State<NewsAndDealsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.red),
+    );
+  }
+}
