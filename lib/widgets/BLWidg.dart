de599039523ad85d11048921772cea416919f956
Arinaho Muleba <arinaomuleluz@gmail.com>
2022-08-18 00:40:56 +0200

    clickable

--- lib/widgets/BLWidg.dart
+++ lib/widgets/BLWidg.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../models/BL.dart';
 import '../models/constants.dart';
 
@@ -10,45 +10,94 @@ class BusinessListingWidg extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-      child: Row(
-        children: [
-          Image.network(
-            BUSINESS_LISTINGS_IMAGE_URL + businessListing.logo,
-            fit: BoxFit.contain,
-            height: 60,
-            width: 60,
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    businessListing.name,
-                    style: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.bold),
+    //BusinessListing businessListing = ;
+    return GestureDetector(
+      onTap: () {
+        showBottomSheet(
+            context: context,
+            builder: ((context) {
+              return SafeArea(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
                   ),
-                  Text(
-                    businessListing.businesType,
-                    style: const TextStyle(),
-                  ),
-                  // ignore: unnecessary_string_interpolations
-                  Text(
-                    "${businessListing.firstNumber} ${businessListing.secondNumber}",
-                    style: const TextStyle(),
-                  ),
-                  (businessListing.email != '')
-                      ? Text(businessListing.address)
-                      : const SizedBox(),
-                  const Divider(),
-                ],
-              ),
+                  height: 400,
+                  child: Center(
+                      child: Column(
+                    children: [
+                      businessListing.firstNumber != ''
+                          ? ListTile(
+                              iconColor: Colors.blue,
+                              leading: const Icon(Icons.phone),
+                              title: Text(businessListing.firstNumber))
+                          : const SizedBox(),
+                      businessListing.secondNumber != ''
+                          ? ListTile(
+                              iconColor: Colors.blue,
+                              leading: const Icon(Icons.phone),
+                              title: Text(businessListing.secondNumber))
+                          : const SizedBox(),
+                      businessListing.email != ''
+                          ? ListTile(
+                              iconColor: Colors.green,
+                              leading: const Icon(Icons.email),
+                              title: Text(businessListing.email))
+                          : const SizedBox(),
+                      CupertinoButton(
+                        color: CupertinoColors.destructiveRed,
+                        child: const Text('Close'),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ],
+                  )),
+                ),
+              );
+            }));
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+        child: Row(
+          children: [
+            Image.network(
+              BUSINESS_LISTINGS_IMAGE_URL + businessListing.logo,
+              fit: BoxFit.contain,
+              height: 60,
+              width: 60,
             ),
-          )
-        ],
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      businessListing.name,
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      businessListing.businesType,
+                      style: const TextStyle(),
+                    ),
+                    // ignore: unnecessary_string_interpolations
+                    Text(
+                      "${businessListing.firstNumber} ${businessListing.secondNumber}",
+                      style: const TextStyle(),
+                    ),
+                    (businessListing.email != '')
+                        ? Text(businessListing.address)
+                        : const SizedBox(),
+                    const Divider(),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
